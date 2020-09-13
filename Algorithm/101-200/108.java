998e1228cc4c3bfbae01a4a0d3967a586cdddf89
haneul99 <healing5746@gmail.com>
2020-09-14 01:24:13 +0900

    108. Convert Sorted Array to Binary Search Tree

--- Algorithm/101-200/108.java
+++ Algorithm/101-200/108.java
@@ -31,6 +31,9 @@ class Solution {
 108. Convert Sorted Array to Binary Search Tree
 https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
 
 middle is root....
 middle index 0~middle-1, middle+1~n
