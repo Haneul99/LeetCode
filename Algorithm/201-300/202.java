5b08ab53958fdeacf3a5378836399b6e044b2617
haneul99 <healing5746@gmail.com>
2020-09-27 00:35:53 +0900

    202. Happy Number

---
+++ Algorithm/201-300/202.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean isHappy(int n) {
+    	HashSet<Integer> set=new HashSet<Integer>();
+        boolean check=false;
+        int sum;
+        while(check==false) {
+    		if(set.contains(n)) break;
+    		set.add(n);
+        	sum=0;
+        	sum+=(n%10)*(n%10);
+        	while(n/10>0) {
+        		n/=10;
+        		sum+=(n%10)*(n%10);
+        	}
+    		if(sum==1) check=true;
+    		n=sum;
+        }  
+        return check;
+	}
+}
+
+/*
+202. Happy Number
+https://leetcode.com/problems/happy-number/
+
+Write an algorithm to determine if a number n is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Return True if n is a happy number, and False if not.
+
+Use HashSet for checking same number(false return)
+with % operation, add numbers
+
+*/
